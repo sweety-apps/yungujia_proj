6e8d9cf3e203a10cb29790a7cef7162de205b3b5
boboxu <bobo__x@126.com>
2012-07-19 20:05:23 +0800

    细化注册的控件

--- yungujia/UI/RegisterViewController.m
+++ yungujia/UI/RegisterViewController.m
@@ -7,12 +7,27 @@
 //
 
 #import "RegisterViewController.h"
+#import "AppDelegate.h"
 
-@interface RegisterViewController ()
+@interface RegisterViewController (private)
 
+-(void)buildDataSource;
 @end
 
 @implementation RegisterViewController
+@synthesize txtTel;
+@synthesize txtName;
+@synthesize txtPassword;
+@synthesize lblUserStyle;
+@synthesize txtIdentifyCode;
+@synthesize txtConfirmPassword;
+@synthesize pickUserStyle;
+@synthesize arrayUserStyle;
+
+-(void)buildDataSource
+{
+    self.arrayUserStyle = [[NSMutableArray alloc] initWithObjects:@"网友",@"房地产从业者",@"金融行业从业者", nil];
+}
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -20,6 +35,8 @@
     if (self) {
         // Custom initialization
         self.title = @"注册";
+        [self buildDataSource];
+        [self.lblUserStyle setText:[self.arrayUserStyle objectAtIndex:0]];
     }
     return self;
 }
@@ -42,4 +59,107 @@
     return (interfaceOrientation == UIInterfaceOrientationPortrait);
 }
 
+-(void)dealloc
+{
+    [self.arrayUserStyle release];
+    [super dealloc];
+}
+
+#pragma mark -pickdelegate 
+// returns width of column and height of row for each component. 
+- (CGFloat)pickerView:(UIPickerView *)pickerView widthForComponent:(NSInteger)component
+{
+    return 320.0;
+}
+
+- (CGFloat)pickerView:(UIPickerView *)pickerView rowHeightForComponent:(NSInteger)component
+{
+    return 40.0;
+}
+// these methods return either a plain UIString, or a view (e.g UILabel) to display the row for the component.
+// for the view versions, we cache any hidden and thus unused views and pass them back for reuse. 
+// If you return back a different object, the old one will be released. the view will be centered in the row rect  
+- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
+{
+    return [self.arrayUserStyle objectAtIndex:row];
+}
+
+//- (UIView *)pickerView:(UIPickerView *)pickerView viewForRow:(NSInteger)row forComponent:(NSInteger)component reusingView:(UIView *)view
+//{
+//    
+//}
+
+- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component
+{
+    NSLog(@"%d",row);
+    [self.lblUserStyle setText:[self.arrayUserStyle objectAtIndex:row]];
+    [self.pickUserStyle setHidden:true];
+}
+
+#pragma mark -pickdatasource
+// returns the number of 'columns' to display.
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView
+{
+    return 1;
+}
+
+
+// returns the # of rows in each component..
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
+{
+    return 3;
+}
+
+
+-(IBAction)actionSendIdentifyCode:(id)sender
+{
+    
+}
+
+-(IBAction)actionRegist:(id)sender
+{
+    NSLog(@"actionRegist");
+    //账户名姓名+电话号码，以防重复
+    NSString* accountname = [NSString stringWithFormat:@"%@+%@",txtName.text,txtTel.text];
+    [(AppDelegate*)[[UIApplication sharedApplication] delegate] ShowMainView:txtName.text weiboaccount:accountname];
+}
+
+-(IBAction)actionClickBtnUserStyle:(id)sender
+{
+    [self.pickUserStyle setHidden:false];
+}
+
+#pragma mark -UITextFieldDelegate
+- (BOOL)textFieldShouldReturn:(UITextField *)textField;              // called when 'return' key pressed. return NO to ignore.
+{
+    if ([[textField text] length] != 0) {
+        switch ([textField tag]) {
+            case 0://电话
+                
+                break;
+            case 1://验证码
+                
+                break;
+            case 2://姓名
+                
+                break;
+            case 3://密码
+                
+                break;
+            case 4://确认密码
+                
+                break;
+            default:
+                break;
+        }
+    }
+    
+    [textField resignFirstResponder];
+    return YES;
+}
+
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string;   // return NO to not change text
+{
+    return YES;
+}
 @end
