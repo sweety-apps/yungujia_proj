2b86c65fc6cd2d706a8a7b64c7aeb97216c4423c
monkeybad <rocker.roller.justin.lee@gmail.com>
2013-02-03 00:57:38 +0800

    aaa

---
+++ TimerCamera/TimerCamera/Modules/Album/AssetOperator.h
@@ -0,0 +1,13 @@
+//
+//  AssetOperator.h
+//  TimerCamera
+//
+//  Created by lijinxin on 13-2-3.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AssetOperator : NSObject
+
+@end
---
+++ TimerCamera/TimerCamera/Modules/Album/AssetOperator.m
@@ -0,0 +1,13 @@
+//
+//  AssetOperator.m
+//  TimerCamera
+//
+//  Created by lijinxin on 13-2-3.
+//
+//
+
+#import "AssetOperator.h"
+
+@implementation AssetOperator
+
+@end
