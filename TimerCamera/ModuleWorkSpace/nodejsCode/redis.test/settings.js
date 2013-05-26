0488cae1051ad3e1bbf85cb44e1e8d347db7a99c
monkeybad <rocker.roller.justin.lee@gmail.com>
2013-05-27 00:07:25 +0800

    diaobaole

---
+++ TimerCamera/ModuleWorkSpace/nodejsCode/redis.test/settings.js
@@ -0,0 +1,12 @@
+module.exports = {
+	redisHost: '127.0.0.1',
+	redisPort: 6379,
+	redisPrefix: 'site_sess_',
+	redisPass: '',
+	redisTtl: 30, //seccend
+	redisDb: 0,
+
+	sessionSecret: 'mypassword',
+	sessionTTL: 30
+};
+
